7fb999660a687b5d5786f5b857170f54bf6e1de2
Ezequiel López <contacto.ezequiel.lopez@gmail.com>
2022-06-13 01:00:08 -0300

    Cambio de color del texto de aviso de dialog_content a verde

--- urbetrack/lib/widgets/reportDialog/dialog_content.dart
+++ urbetrack/lib/widgets/reportDialog/dialog_content.dart
@@ -2,11 +2,11 @@ import 'package:flutter/material.dart';
 import 'form_dialog.dart';
 
 class DialogContent extends StatelessWidget {
-    const DialogContent({
+  const DialogContent({
     Key? key,
     required TextEditingController characterNameController,
     required GlobalKey<FormState> formKey,
-    this.isReportCreated =false,
+    this.isReportCreated = false,
   })  : _characterNameController = characterNameController,
         _formKey = formKey,
         super(key: key);
@@ -23,7 +23,13 @@ class DialogContent extends StatelessWidget {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Text(isReportCreated ? 'Reporte creado exitosamente.' : ''),
+          Text(
+            isReportCreated ? 'Reporte creado exitosamente.' : '',
+            style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: isReportCreated ? Colors.green : Colors.black),
+          ),
           ReportDialogForm(
             characterNameController: _characterNameController,
             reportDialogKey: _formKey,
